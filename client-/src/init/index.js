449212d01004f14e0cb2da565e07f9222fdd447e
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2021-08-11 11:15:08 +0200

    remove extra client

--- client-/src/init/index.js
+++
@@ -1,7 +0,0 @@
-import { hello } from '../api-calls/hello.js';
-import { header } from '../components/header.js';
-
-hello().then((greeting) => {
-  const renderedGreeting = header(greeting);
-  document.body.appendChild(renderedGreeting);
-});
